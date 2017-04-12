44fd5e72fa065333a8f23a6614eb93b56483f85f
Sam Gillingham <gillingham.sam@gmail.com>
2017-04-12 13:13:14 +1000

    rollback to PyQt4 version (revision 257683ab169d32da6bff6c90e081fe940700c6ea) so we can have a pt4 branch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,45 +1,29 @@
 #!/bin/bash
 
-set -e # Abort on error.
+if [ $(uname) == Linux ]; then
+    export QMAKESPEC="linux-g++"
 
-export PING_SLEEP=30s
-export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export BUILD_OUTPUT=$WORKDIR/build.out
+    # Add qt.conf to the right place in $SRC_DIR so that
+    # configure.py can run correctly
+    cp $PREFIX/bin/qt.conf $SRC_DIR
+elif [ $(uname) == Darwin ]; then
+    export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib/
 
-touch $BUILD_OUTPUT
+    # Add qt.conf to the right place in $SRC_DIR so that
+    # configure.py can run correctly
+    QTCONF_PLACE=$SRC_DIR/qtdirs.app/Contents/Resources
+    mkdir -p $QTCONF_PLACE
+    cp $PREFIX/bin/qt.conf $QTCONF_PLACE
+fi
 
-dump_output() {
-   echo Tailing the last 500 lines of output:
-   tail -500 $BUILD_OUTPUT
-}
-error_handler() {
-  echo ERROR: An error was encountered with the build.
-  dump_output
-  exit 1
-}
-
-# If an error occurs, run our error handler to output a tail of the build.
-trap 'error_handler' ERR
-
-# Set up a repeating loop to send some output to Travis.
-bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
-PING_LOOP_PID=$!
-
-## START BUILD
 $PYTHON configure.py \
-        --verbose \
-        --confirm-license \
-        --assume-shared \
-        -q $PREFIX/bin/qmake
+          --verbose \
+          --confirm-license \
+          --bindir=$PREFIX/bin \
+          --destdir=$SP_DIR \
+          --qmake=$PREFIX/bin/qmake
 
-make -j$CPU_COUNT >> $BUILD_OUTPUT 2>&1
-make check >> $BUILD_OUTPUT 2>&1
-make install >> $BUILD_OUTPUT 2>&1
+make
+make install
 
-## END BUILD
-
-# The build finished without returning an error so dump a tail of the output.
-dump_output
-
-# Nicely terminate the ping output loop.
-kill $PING_LOOP_PID
+rm -rf $SP_DIR/__pycache__
