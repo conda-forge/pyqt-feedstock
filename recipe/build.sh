9c31299a090d448fd5591238a83ed9914025b5be
Ray Donnelly <mingw.android@gmail.com>
2018-03-12 00:20:16 +0000

    Update to 5.9.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,39 +2,9 @@
 
 set -e # Abort on error.
 
-# Avoid Xcode, cannot put in ${SRC_DIR} as there is a dir called 'sip'
-# there which shadows the sip excutable.
-mkdir xcode
-pushd xcode
-  cp "${RECIPE_DIR}"/xcrun .
-  cp "${RECIPE_DIR}"/xcodebuild .
-  PATH=${PWD}:${PATH}
-popd
-
-if [[ 0 == 1 ]]; then
-export PING_SLEEP=30s
-export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export BUILD_OUTPUT=$WORKDIR/build.out
-
-touch $BUILD_OUTPUT
-
-dump_output() {
-   echo Tailing the last 500 lines of output:
-   tail -500 $BUILD_OUTPUT
-}
-error_handler() {
-  echo ERROR: An error was encountered with the build.
-  dump_output
-  kill $PING_LOOP_PID
-  exit 1
-}
-
-# If an error occurs, run our error handler to output a tail of the build.
-trap 'error_handler' ERR
-
-# Set up a repeating loop to send some output to Travis.
-bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
-PING_LOOP_PID=$!
+# Avoid Xcode
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  PATH=${PREFIX}/bin/xc-avoidance:${PATH}
 fi
 
 # Dumb .. is this Qt or PyQt's fault? (or mine, more likely).
@@ -53,16 +23,6 @@ $PYTHON configure.py \
         --confirm-license \
         --assume-shared \
         -q ${PREFIX}/bin/qmake
-make -j${CPU_COUNT} ${VERBOSE_AT} # >> $BUILD_OUTPUT 2>&1
-make check # >> $BUILD_OUTPUT 2>&1
-make install # >> $BUILD_OUTPUT 2>&1
-
-if [[ 0 == 1 ]]; then
-## END BUILD
-
-# The build finished without returning an error so dump a tail of the output.
-dump_output
-
-# Nicely terminate the ping output loop.
-kill $PING_LOOP_PID
-fi
+make -j${CPU_COUNT} ${VERBOSE_AT}
+make check
+make install
