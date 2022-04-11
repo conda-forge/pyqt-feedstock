aabeb7c0ceb93b7e91b659d1536577066ea0736e
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2022-04-11 11:24:18 +0200

    Address Windows bat failures
    Co-authored-by: h-vetinari <h.vetinari@gmx.com>

--- recipe/bld-pyqt-sip.bat
+++ recipe/bld-pyqt-sip.bat
@@ -1,2 +1,3 @@
 pushd pyqt_sip
 %PYTHON% -m pip install . -vv
+if %ERRORLEVEL% neq 0 exit 1
--- recipe/bld-pyqt.bat
+++ recipe/bld-pyqt.bat
@@ -5,12 +5,15 @@ set PATH=%cd%\jom;%PATH%
 set PATH=%LIBRARY_INC%;%PATH%
 
 call sip-build ^
---verbose ^
---confirm-license ^
---disable QtNfc ^
---no-make
+    --verbose ^
+    --confirm-license ^
+    --disable QtNfc ^
+    --no-make
+if %ERRORLEVEL% neq 0 exit 1
 
 pushd build
 
 jom
+if %ERRORLEVEL% neq 0 exit 1
 jom install
+if %ERRORLEVEL% neq 0 exit 1
