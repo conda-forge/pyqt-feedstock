271fcc1e248c1f9bb8370558b0a38939e3456bd2
Ista Zahn <istazahn@gmail.com>
2022-07-18 18:15:18 -0400

    bugfix

--- recipe/bld-pyqt.bat
+++ recipe/bld-pyqt.bat
@@ -21,5 +21,5 @@ if %ERRORLEVEL% neq 0 exit 1
 :: remove qtwebkit stuff, this is packaged separately by bld_pyqtwebkit.bat
 md  %TEMP%\pyqtwebkit
 move %SP_DIR%\PyQt5\QtWebKit*.so %TEMP%\pyqtwebkit\
-xmove %SP_DIR%\PyQt5\bindings\QtWebKit %TEMP%\pyqtwebkit\
+move %SP_DIR%\PyQt5\bindings\QtWebKit %TEMP%\pyqtwebkit\
 
