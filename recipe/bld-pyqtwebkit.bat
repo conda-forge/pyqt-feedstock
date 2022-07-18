875aa69c218a4014da4bed5a25f24539db025a43
Ista Zahn <istazahn@gmail.com>
2022-07-18 06:28:15 -0400

    bugfix

--- recipe/bld-pyqtwebkit.bat
+++ recipe/bld-pyqtwebkit.bat
@@ -2,4 +2,4 @@
 md %SP_DIR%\PyQt5
 md %SP_DIR%\PyQt5\bindings
 move %TEMP%\pyqtwebkit\QtWebKit*.so %SP_DIR%\PyQt5\ 
-xmove %TEMP%\pyqtwebkit\QtWebKit %SP_DIR%\PyQt5\bindings\
+move %TEMP%\pyqtwebkit\QtWebKit %SP_DIR%\PyQt5\bindings\
