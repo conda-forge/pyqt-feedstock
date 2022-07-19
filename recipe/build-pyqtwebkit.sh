ca504b91d73a4422745df4c93fdced131e44ec6a
Ista Zahn <istazahn@gmail.com>
2022-07-18 21:29:05 -0400

    bugfix

--- recipe/build-pyqtwebkit.sh
+++ recipe/build-pyqtwebkit.sh
@@ -2,5 +2,5 @@ set -exou
 
 ## install pyqtwebkit
 mkdir -p ${SP_DIR}/PyQt5/bindings
-cp /tmp/pyqtwebkit/QtWebKit* ${SP_DIR}/PyQt5/ 
-cp -r /tmp/pyqtwebkit/QtWebKit ${SP_DIR}/PyQt5/bindings/
+mv /tmp/pyqtwebkit/QtWebKit ${SP_DIR}/PyQt5/bindings/
+mv /tmp/pyqtwebkit/QtWebKit* ${SP_DIR}/PyQt5/ 
