c1081761671407c25e7a27d0f3dd4a56ec1f6213
Jarrett Johnson <jarrett.johnson@schrodinger.com>
2025-03-19 14:26:04 -0400

    Disable PyQt_Vulkan on MacArm64

--- recipe/build-pyqt.sh
+++ recipe/build-pyqt.sh
@@ -33,12 +33,15 @@ fi
 if [[ $(uname) == "Darwin" ]]; then
     # Use xcode-avoidance scripts
     export PATH=$PREFIX/bin/xc-avoidance:$PATH
+    if [[ "$(uname -m)" == "arm64" ]]; then
+        EXTRAFLAGS="${EXTRA_FLAGS} --disabled-feature=PyQt_Vulkan"
+    fi
 fi
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
   SIP_COMMAND="$BUILD_PREFIX/bin/python -m sipbuild.tools.build"
   SITE_PKGS_PATH=$($PREFIX/bin/python -c 'import site;print(site.getsitepackages()[0])')
-  EXTRA_FLAGS="--target-dir $SITE_PKGS_PATH"
+  EXTRA_FLAGS="${EXTRA_FLAGS} --target-dir $SITE_PKGS_PATH"
   ln -s ${BUILD_PREFIX}/bin/qmake6 ${BUILD_PREFIX}/bin/qmake
 fi
 ln -s ${PREFIX}/bin/qmake6 ${PREFIX}/bin/qmake
